ba2c41d50c06ee5de3f40e8509f588697175962c
KOHHyeonJun <tttmmm100@naver.com>
2023-05-15 16:50:02 +0900

    Step 17 파일 수정

--- Step17_JDBC/src/test/main/MainClass10.java
+++ Step17_JDBC/src/test/main/MainClass10.java
@@ -1,10 +1,10 @@
 package test.main;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 import test.dto.MemberDto;
+import test.util.DBConnect;
 
 public class MainClass10 {	
 	public static void main(String[] args) {
@@ -16,30 +16,18 @@ public class MainClass10 {
 		m.setName(name);
 		m.setAddr(addr);
 		m.setNum(num);
-		MainClass10.update(m);
+		update(m);
 		
 	}
 	
 	public static void update(MemberDto dto) {
-		
-		//DB연결객체를 담을 지역 변수 만들기
-	      Connection conn=null;
-	      
-	      try {
-	         //오라클 드라이버 로딩
-	         Class.forName("oracle.jdbc.driver.OracleDriver");
-	         //접속할 DB의 정보@아이피주소 : port번호 :db이름
-	         String url="jdbc:oracle:thin:@localhost:1521:xe";
-	         conn=DriverManager.getConnection(url, "scott", "tiger");
-	         System.out.println("Oracle DB접속 성공");
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
-
+		  Connection conn=null;
 	      PreparedStatement pstmt=null;
 	      try {
-			//실행할 미완성의 sql문
-	    	String sql="update member"
+	    	  //Connection 객체의 참조값 얻어오기
+	    	  conn=new DBConnect().getConn();
+	    	  //실행할 미완성의 sql문
+	    	  String sql="update member"
 	    			+ " set name=?,addr=?"
 	    			+ " where num=?";
 	    	//미완성의 sql 문을 전달하면서 PreparedStatement 객채의 참조값 얻어내기
