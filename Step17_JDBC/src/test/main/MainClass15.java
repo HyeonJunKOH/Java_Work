03c356eb82209c55ebb0ff8d31e3b8e7acfabc23
KOHHyeonJun <tttmmm100@naver.com>
2023-05-16 15:16:58 +0900

    Step17_MainClass13~17 추가

---
+++ Step17_JDBC/src/test/main/MainClass15.java
@@ -0,0 +1,21 @@
+package test.main;
+
+import test.dao.MemberDao;
+
+
+public class MainClass15 {
+	public static void main(String[] args) {
+		//삭제할 회원의 정보
+		int num=2;
+		
+		
+		//위의 정보를 이용해서 회원정보를 삭제하고 성공이면 삭제했습니다.가 출력되도록 해보세요.
+		
+		
+		boolean isdelete = new MemberDao().delete(num);
+		if(isdelete) {
+			System.out.println("삭제했습니다.");
+		}
+		
+	}
+}
